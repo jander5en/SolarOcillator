0fe5ba33a6c437c6ab33369f63fd6fc261c1ad93
Jonas <jonas341278@gmail.com>
2018-11-27 21:11:22 +0100

    Ny variabel i movePattern

--- SolarOcillator.ino
+++ SolarOcillator.ino
@@ -7,8 +7,6 @@ int lysverdi = A0;
 int maxVal = 0;
 int sensorValue = 0;
 
-/* alle disse Serial.print brukes eller har vært brukt til debugging.
-   Håper du kan unnskylde at det blir litt rotete ;-) */
 
 //Verdien som gir utgangspunt til sektorene 
 const int radius = 90;
@@ -24,7 +22,7 @@ void setup(){
     //sette opp serial 
     Serial.begin(9600);
     //roterer motorene til utgangsposisjon
-    movePattern(origo[0],origo[1]);
+    movePattern(origo[0],origo[1], speed);
 }
 
 void loop(){
@@ -33,9 +31,12 @@ void loop(){
     if(quadCounter <= 8 ){
       	searchPattern();
     }
+	else{
+		
+	}
 }
 
-void searchPattern(){
+void searchPattern(int speed){
     /*Søker etter høyeste effekt fra sensor,
       Juster posisjon, registrer verdier fra sensor, 
       evaluer og juster origo hvis høyere verdi finnes
@@ -44,29 +45,19 @@ void searchPattern(){
     int patternResult[4];
     //setter opp variabelen som brukes til å gi verdier relativ til origo
     int modulatorlissom = floor(radius/quadCounter);
-    //Serial.print("Soekeboksradius?: ");
-    //Serial.println(modulatorlissom);
 
     //Roter motorene relativt til origo og mål effekten i punktene
-    movePattern(origo[0] + modulatorlissom, origo[1] + modulatorlissom);
+    movePattern(origo[0] + modulatorlissom, origo[1] + modulatorlissom, speed);
     patternResult[0] = analogRead(lysverdi);
-    //Serial.print("Sensor output 0: ");
-    //Serial.println(patternResult[0]);
 
-    movePattern(origo[0] - modulatorlissom, origo[1] - modulatorlissom);
+    movePattern(origo[0] - modulatorlissom, origo[1] - modulatorlissom, speed);
+    patternResult[1] = analogRead(lysverdi);
+
+    movePattern(origo[0] - modulatorlissom, origo[1] + modulatorlissom, speed);
     patternResult[2] = analogRead(lysverdi);
-    //Serial.print("Sensor output 2: ");
-    //Serial.println(patternResult[2]);
 
-    movePattern(origo[0] - modulatorlissom, origo[1] + modulatorlissom);
-    patternResult[3] = analogRead(lysverdi);
-    //Serial.print("Sensor output 3: ");
-    //Serial.println(patternResult[3]);
-
-    movePattern(origo[0] + modulatorlissom, origo[1] - modulatorlissom);
-    patternResult[1] = analogRead(lysverdi); 
-    //Serial.print("Sensor output 1: ");
-    //Serial.println(patternResult[1]); 
+    movePattern(origo[0] + modulatorlissom, origo[1] - modulatorlissom, speed);
+    patternResult[3] = analogRead(lysverdi); 
 
     //Finn den av posisjonene som hadde den høyeste verdien
     maxVal = 0;
@@ -83,28 +74,28 @@ void searchPattern(){
       	case 1:
         	origo[0] = origo[0] + modulatorlissom;
         	origo[1] = origo[1] + modulatorlissom;
-        	movePattern(origo[0],origo[1]);
+        	movePattern(origo[0],origo[1], speed);
         	//Serial.println("Case 1");
       		break;
 
         case 2:
         	origo[0] = origo[0] + modulatorlissom;
         	origo[1] = origo[1] - modulatorlissom;
-        	movePattern(origo[0],origo[1]);
+        	movePattern(origo[0],origo[1], speed);
         	//Serial.println("Case 2");
       		break;
 
         case 3:
         	origo[0] = origo[0] - modulatorlissom;
         	origo[1] = origo[1] - modulatorlissom;
-        	movePattern(origo[0],origo[1]);
+        	movePattern(origo[0],origo[1], speed);
         	//Serial.println("Case 3");
       		break;
 
         case 4:
         	origo[0] = origo[0] - modulatorlissom;
         	origo[1] = origo[1] + modulatorlissom;
-        	movePattern(origo[0],origo[1]);
+        	movePattern(origo[0],origo[1], speed);
         	//Serial.println("Case 4");
       		break;
 
@@ -113,7 +104,7 @@ void searchPattern(){
         	//Serial.print("Ingen ny origo. Origo i: ");
         	//Serial.println(origo[0]);
         	//Serial.println(origo[1]);
-        	movePattern(origo[0],origo[1]);
+        	movePattern(origo[0],origo[1], speed);
       		break;
     }
     //Serial.println(origo[0]);
@@ -122,7 +113,7 @@ void searchPattern(){
   	quadCounter = quadCounter + 1;
 }
 
-void movePattern(int serv1, int serv2){
+void movePattern(int serv1, int serv2, int speed){
     /*Tar inn vinkler og vender motorene */
     for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
       	// Ta inn verdiene for nåværende posisjon for motorene
@@ -136,17 +127,20 @@ void movePattern(int serv1, int serv2){
       	}else if(currentPos1 > serv1){
         	myServo1.write(currentPos1 - 1);                       
       	}
-      	delay(3);
+      	delay(speed);
       	// rotere servo 2 ett steg mot målposisjonen gitt ved serv2:
       	if(currentPos2 < serv2){
         	myServo2.write(currentPos2 + 1);
       	}else if(currentPos2 > serv2){
         	myServo2.write(currentPos2 - 1);                       
       	}
-      	delay(3);     
+      	delay(speed);     
     }
     /*Legger inn en pause for at motorene skal kunne gjennomføre 
       rotasjonen før målingen tas. kan vi f.eks måle når motorene 
       slutter å gå og hente data fra panelet da?*/
 }
+
+
+
 
